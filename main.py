b42e074e5e025ffbe31cf068264545159462a89f
RakaAF123 <rakaadmiharfan@gmail.com>
2023-12-08 22:25:47 +0700

    middleware update

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from routes.requirements import getter_router, admin_router, friend_router, support_router
 from routes.auth import auth_router
 from jose import JWTError, jwt
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# Setelan CORS untuk menerima permintaan dari semua domain
+origins = ["*"]
+
+# Tambahkan middleware CORS ke aplikasi
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(friend_router, prefix="/friend")
 app.include_router(support_router, prefix="/support")
 app.include_router(getter_router, prefix="/getters")
